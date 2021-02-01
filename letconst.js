7eff73337312814f4feff3d7282df67b32ea82c9
Rony <rafiqulrony2015u@gmaile.com>
2021-02-01 22:11:04 +0600

    first

---
+++ letconst.js
@@ -0,0 +1,16 @@
+const name = "name one";
+console.log(name);
+
+const numbers = [12, 49];
+numbers[1] = 88;
+numbers.push = (12);
+
+let patientName = "abc name";
+patientName = "aaa name";
+console.log(patientName);
+
+let sum = 0;
+for(var i = 0; i < 10; i++){
+    sum = sum + i;
+}
+console.log(i);
